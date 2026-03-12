a1d247611204b6849885c97aebc16c00ff2ea80b
noah <bbrawn@bbcag.ch>
2026-03-12 14:06:44 +0100

    url import fix

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,13 +1,4 @@
-const URL = process.env.API_URL;
+const URL = import.meta.env.API_URL;
 
 console.log(`Api Url ::::::${URL}`);
 
-export async function signUp(data) {
-  return await fetch(`${URL}/auth/signup`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data),
-  })
-    .then(checkResponse)
-    .then((res) => res.json());
-}
