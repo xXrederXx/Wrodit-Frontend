e0a7380f810c1bc3dd268ae0007bf833726146ce
noah <bbrawn@bbcag.ch>
2026-03-12 15:58:18 +0100

    env variable

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,8 +1,18 @@
-const URL = import.meta.env.API_URL;
+const URL = import.meta.env.VITE_API_URL;
 
 //const TURL = "http://xcwkwswkso04gs40k8g48k8w.207.180.221.9.sslip.io"
 
+console.log(import.meta.env);
 console.log(`Api Url: ${URL}`);
+
+export async function signUp(data) {
+  return await fetch(`${URL}/auth/signup`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json());
+}
 
 
 export async function fetchhome() {
