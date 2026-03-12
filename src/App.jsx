73df0572e5964c2e5a1649e5d23d92cc58e5f237
noah <bbrawn@bbcag.ch>
2026-03-12 09:11:14 +0100

    Adding Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Outlet } from "react-router";
-import styles from "./App.css";
 
 export default function App() {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,57 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+
 import "./index.css";
 import App from "./App.jsx";
+import HomeRoute from "./routes/homeRoute.jsx";
+import PostRoute from "./routes/PostRoute.jsx";
+import ThreadRoute from "./routes/ThreadRoute.jsx";
+import UserRoute from "./routes/UserRoute.jsx";
+import RegisterRoute from "./routes/RegisterRoute.jsx";
+import LoginRoute from "./routes/LoginRoute.jsx";
+import CreatePostRoute from "./routes/CreatePostRoute.jsx";
+import CreateThreadRoute from "./routes/CreateThreadRoute.jsx";
+
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [{}],
+    children: [
+      {
+        index: true,
+        element: <HomeRoute />,
+      },
+      {
+        path: "wrodit/post/:id",
+        element: <PostRoute />,
+      },
+      {
+        path: "wrodit/thread/:id",
+        element: <ThreadRoute />,
+      },
+      {
+        path: "wrodit/register",
+        element: <RegisterRoute />,
+      },
+      {
+        path: "wrodit/login",
+        element: <LoginRoute />,
+      },
+      {
+        path: "wrodit/user/:id",
+        element: <UserRoute />,
+      },
+      {
+        path: "wrodit/create/post",
+        element: <CreatePostRoute />,
+      },
+      {
+        path: "wrodit/create/thread",
+        element: <CreateThreadRoute />,
+      },
+    ],
   },
 ]);
 
