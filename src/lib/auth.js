42a7886934879e38e9d597216938bb9cea19c9f5
noah <bbrawn@bbcag.ch>
2026-03-12 15:25:49 +0100

    Api corse request test

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,4 +1,20 @@
 const URL = import.meta.env.API_URL;
 
-console.log(`Api Url ::::::${URL}`);
+//const TURL = "http://xcwkwswkso04gs40k8g48k8w.207.180.221.9.sslip.io"
 
+console.log(`Api Url: ${URL}`);
+
+export async function signUp(data) {
+  return await fetch(`${URL}/auth/signup`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  })
+    .then(checkResponse)
+    .then((res) => res.json());
+}
+
+
+export async function fetchhome() {
+  return await fetch(`${URL}/home/`)
+}
