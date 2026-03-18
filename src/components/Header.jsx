5c4a628a1276aca1ae5aef4ea715976d23c99ed8
noah <bbrawn@bbcag.ch>
2026-03-18 14:17:37 +0100

    adding sidebar corectly to hrader

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,8 @@
-import { Link } from "react-router-dom";
 import SideBar from "./SideBar";
 export default function Header() {
   return (
     <header>
       <SideBar />
-      <Link to="wrodit/register">Register</Link>
-      <Link to="wrodit/login">Anmelden</Link>
     </header>
   );
 }
