71d372e6189135ef61d4618ebe0767e683b88ee8
noah <bbrawn@bbcag.ch>
2026-03-20 14:57:31 +0100

    adding post form

--- src/routes/CreatePostRoute.jsx
+++ src/routes/CreatePostRoute.jsx
@@ -1,3 +1,10 @@
+import PostForm from "../components/PostForm";
+
 export default function CreatePostRoute() {
-    return <h1>CreatePostRoute</h1>
+  return (
+    <>
+      <h1>CreatePostRoute</h1>
+      <PostForm/>
+    </>
+  );
 }
