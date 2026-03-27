365e10b494afb21aede18703150ff41c91606c19
Thierry Morgenthaler <thierry.morgenthaler@bbcag.ch>
2026-03-27 10:18:58 +0100

    feat(md): add markdown support to post text

--- src/components/PostText.jsx
+++ src/components/PostText.jsx
@@ -1,7 +1,19 @@
-export default function PostText({text}) {
-    return(
-        <>
-        <p>{text} Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam pariatur ipsam sequi ullam accusamus, fugit nisi excepturi assumenda, error sint possimus totam minima quas ut alias nesciunt veritatis molestias neque!</p>
-        </>
-    )
+import { marked } from "marked";
+
+export default function PostText({ text }) {
+  return (
+    <>
+      
+      <div
+        dangerouslySetInnerHTML={{ __html: marked.parse(text) }}
+      />
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+        pariatur ipsam sequi ullam accusamus, fugit nisi excepturi assumenda,
+        error sint possimus totam minima quas ut alias nesciunt veritatis
+        molestias neque!
+      </p>
+    </>
+  );
 }
