5f6280d58d21f10a68fec5cf8fcac164c45e6a6f
noah <bbrawn@bbcag.ch>
2026-03-30 14:59:23 +0200

    only showing edit button when right user

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,17 +2,24 @@ import { useEffect, useState } from "react";
 import styles from "./Comment.module.css";
 import PostFooter from "./PostFooter";
 import PostInformation from "./PostInformation";
-import { fetchCommentByParent, fetchUser } from "../lib/wrodit";
+import {
+  fetchAllUserData,
+  fetchCommentByParent,
+  fetchUser,
+} from "../lib/wrodit";
 import { Link } from "react-router-dom";
+import CommentEditButton from "./CommentEditButton";
 
-export default function Comment({ name, postId, data, lvl }) {
+export default function Comment({ name, postId, data, lvl, isEditValid, commentId }) {
   const [children, setChildren] = useState([]);
   const [error, setError] = useState("");
+  const [self, setSelf] = useState(false);
 
   useEffect(() => {
     const fetchComments = async () => {
       try {
         const newData = await fetchCommentByParent(data.id);
+        const user = await fetchAllUserData();
 
         const childUsername = await Promise.all(
           newData.content.map(async (item) => {
@@ -25,6 +32,7 @@ export default function Comment({ name, postId, data, lvl }) {
           }),
         );
 
+        setSelf(user);
         setChildren(childUsername);
       } catch (err) {
         setError(err.message || "Fehler beim Laden");
@@ -37,31 +45,48 @@ export default function Comment({ name, postId, data, lvl }) {
     }
   }, [data.id]);
 
+
   return (
+    
     <>
       <div style={{ display: "flex" }}>
         <div style={{ flex: lvl }}></div>
+
         <div className={styles.comment} style={{ flex: 10, minWidth: "20rem" }}>
+
           <PostInformation name={name} createdAt={data.createdAt} />
+
           <p>{data.content}</p>
           <PostFooter vote={data.votes} />
+
           <Link
             to={`/wrodit/create/comment/parent/${postId}/${data.id}`}
             className={styles.linkButton}
           >
             Komentieren
           </Link>
+          
+          <CommentEditButton postId={commentId} isValid={isEditValid}/>
+
         </div>
+
       </div>
-      {children.map((child) => (
-        <Comment
-          name={child.username}
-          postId={postId}
-          key={child.id}
-          data={child}
-          lvl={lvl + 1}
-        />
-      ))}
+      {children.map((child) => {
+        
+
+        const isValid = self.id === child.userId;
+
+        return (
+          <Comment
+            name={child.username}
+            postId={postId}
+            commentId={child.id}
+            data={child}
+            lvl={lvl + 1}
+            isEditValid={isValid}
+          />
+        );
+      })}
     </>
   );
 }
