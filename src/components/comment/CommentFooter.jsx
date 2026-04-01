87ff02f280c244c4903ec5f231d2dc314aa4baa0
Thierry Morgenthaler <thierry.morgenthaler@bbcag.ch>
2026-04-01 11:19:36 +0200

    refactor comment footer

--- src/components/comment/CommentFooter.jsx
+++ src/components/comment/CommentFooter.jsx
@@ -1,23 +1,17 @@
 import { FcLike } from "react-icons/fc";
 import { AiOutlineHeart, AiOutlineDislike, AiFillDislike } from "react-icons/ai";
 import styles from "../post/PostFooter.module.css";
-import {
-  DislikeLikeComment,
-  fetchSelfLikesComment,
-  likeComment,
-  RemoveLikeComment,
-} from "../../lib/wrodit.js";
+import { fetchSelfLikesComment, likeComment } from "../../lib/wrodit.js";
 import { useEffect, useState } from "react";
 
 export default function CommentFooter({ vote, commentId }) {
-  const [self, setSelf] = useState(0);
-  const [error, setError] = useState(""); // eslint-disable-line
+  const [userVote, setUserVote] = useState(0);
 
   useEffect(() => {
     const fetchLikes = async () => {
       try {
         const likeSelf = await fetchSelfLikesComment(commentId);
-        setSelf(likeSelf.vote ? likeSelf.vote : 0);
+        setUserVote(likeSelf.vote ? likeSelf.vote : 0);
       } catch (err) {
         setError(err.message || "Fehler beim Laden");
       }
@@ -25,28 +19,10 @@ export default function CommentFooter({ vote, commentId }) {
     fetchLikes();
   }, [commentId]);
 
-  const handleLike = async () => {
+  const handleLike = async value => {
     try {
-      await likeComment(commentId);
-      setSelf(1);
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  const handleRemoveLike = async () => {
-    try {
-      await RemoveLikeComment(commentId);
-      setSelf(0);
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  const handleDisLike = async () => {
-    try {
-      await DislikeLikeComment(commentId);
-      setSelf(-1);
+      await likeComment(commentId, value);
+      setUserVote(value);
     } catch (err) {
       console.error(err);
     }
@@ -54,13 +30,13 @@ export default function CommentFooter({ vote, commentId }) {
 
   return (
     <footer className={styles.footer}>
-      {vote + self}
-      {self === 1 ?
-        <FcLike onClick={handleRemoveLike} />
-      : <AiOutlineHeart onClick={handleLike} />}
-      {self === -1 ?
-        <AiFillDislike onClick={handleRemoveLike} />
-      : <AiOutlineDislike onClick={handleDisLike} />}
+      {vote + userVote}
+      {userVote === 1 ?
+        <FcLike onClick={() => handleLike(0)} />
+      : <AiOutlineHeart onClick={() => handleLike(1)} />}
+      {userVote === -1 ?
+        <AiFillDislike onClick={() => handleLike(0)} />
+      : <AiOutlineDislike onClick={() => handleLike(-1)} />}
     </footer>
   );
 }
