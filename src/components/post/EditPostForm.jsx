b14186ab2edef0d5147df1632ef7836a6259999e
Thierry Morgenthaler <thierry.morgenthaler@bbcag.ch>
2026-04-01 10:35:11 +0200

    move into post

--- src/components/post/EditPostForm.jsx
+++ src/components/post/EditPostForm.jsx
@@ -1,8 +1,8 @@
 import { Form } from "react-router";
-import Button from "./ui/Button.jsx";
-import Input from "./ui/Input.jsx";
-import Text from "./ui/Text.jsx";
-import styles from "./SignUpForm.module.css";
+import Button from "../ui/Button.jsx";
+import Input from "../ui/Input.jsx";
+import Text from "../ui/Text.jsx";
+import styles from "../SignUpForm.module.css";
 import { useEffect, useState } from "react";
 
 export default function EditPostForm({ onCancel, errors = {}, title, content }) {
