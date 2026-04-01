c70f631f62f325fa8b6f79fece4e9702d709c597
Thierry Morgenthaler <thierry.morgenthaler@bbcag.ch>
2026-04-01 11:16:37 +0200

    extract time ago function

--- src/components/post/PostInformation.jsx
+++ src/components/post/PostInformation.jsx
@@ -1,21 +1,6 @@
 import styles from "./PostInformation.module.css";
 import { Link } from "react-router-dom";
-
-function timeAgo(createdAt) {
-  const now = new Date();
-  const postDate = new Date(createdAt);
-  const diffMs = now - postDate; // Differenz in Millisekunden
-
-  const diffSec = Math.floor(diffMs / 1000);
-  const diffMin = Math.floor(diffSec / 60);
-  const diffHours = Math.floor(diffMin / 60);
-  const diffDays = Math.floor(diffHours / 24);
-
-  if (diffSec < 60) return `${diffSec} Sekunden`;
-  if (diffMin < 60) return `${diffMin} Minuten`;
-  if (diffHours < 24) return `${diffHours} Stunden`;
-  return `${diffDays} Tagen`;
-}
+import { timeAgo } from "../../lib/util.js";
 
 export default function PostInformation({ username, threadId, threadName, createdAt }) {
   return (
