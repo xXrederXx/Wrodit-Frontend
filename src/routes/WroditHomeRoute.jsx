8e67e74bc2e51fe4d88875f1ae2e95984c845762
Thierry Morgenthaler <thierry.morgenthaler@bbcag.ch>
2026-04-01 11:39:36 +0200

    refactor home route

--- src/routes/WroditHomeRoute.jsx
+++ src/routes/WroditHomeRoute.jsx
@@ -1,59 +1,46 @@
 import PostBox from "../components/post/PostBox.jsx";
 import { fetchPosts, fetchUser, fetchThread } from "../lib/wrodit";
-import { useLoaderData, redirect } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 
 async function clientLoader() {
-  try {
-    const postsData = await fetchPosts(true);
+  const postsData = await fetchPosts();
 
-    const postsWithData = await Promise.all(
-      postsData.content.map(async post => {
-        const [userData, threadData] = await Promise.all([
-          fetchUser(post.userId),
-          fetchThread(post.threadId),
-        ]);
+  const postsWithData = await Promise.all(
+    postsData.content.map(async post => {
+      const [userData, threadData] = await Promise.all([
+        fetchUser(post.userId),
+        fetchThread(post.threadId),
+      ]);
 
-        return {
-          ...post,
-          userLoaderData: userData,
-          threadLoaderData: threadData,
-        };
-      }),
-    );
+      return {
+        ...post,
+        userLoaderData: userData,
+        threadLoaderData: threadData,
+      };
+    }),
+  );
 
-    return { ...postsData, content: postsWithData };
-  } catch (err) {
-    console.error("ERROR:", err.status);
-
-    if (err.status === 401) {
-      return redirect("/wrodit/login");
-    }
-    throw err;
-  }
-};
+  return { ...postsData, content: postsWithData };
+}
 
 export default function WroditHomeRoute() {
   const data = useLoaderData();
   const posts = data.content;
 
-  return (
-    <>
-      {posts.map((post, index) => (
-        <PostBox
-          key={index}
-          userLoaderData={post.userLoaderData}
-          createdAt={post.createdAt}
-          title={post.title}
-          text={post.content}
-          vote={post.vote}
-          name={post.userLoaderData ? post.userLoaderData.username : "Loading..."}
-          threadId={post.threadId}
-          threadName={post.threadLoaderData ? post.threadLoaderData.name : "Loading..."}
-          to={post.id}
-        />
-      ))}
-    </>
-  );
+  return posts.map(post => (
+    <PostBox
+      key={post.id}
+      userLoaderData={post.userLoaderData}
+      createdAt={post.createdAt}
+      title={post.title}
+      text={post.content}
+      vote={post.vote}
+      username={post.userLoaderData ? post.userLoaderData.username : "Loading..."}
+      threadId={post.threadId}
+      threadName={post.threadLoaderData ? post.threadLoaderData.name : "Loading..."}
+      to={post.id}
+    />
+  ));
 }
 
 WroditHomeRoute.loader = clientLoader;
