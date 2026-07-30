2d8f0361ab8e0adcefe125b6f63d09357125b4b0
xXrederXx <morgenthalerthierry@gmail.com>
2026-07-30 15:30:40 +0200

    feat: add a cache cleaner

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router";
 
 import Header from "./components/Header.jsx";
+import BackgroundCacheCleaner from "./components/BackgroundCacheCleaner.jsx";
 
 export default function App() {
   return (
     <>
+      <BackgroundCacheCleaner />
       <Header />
       <main>
         <Outlet />
---
+++ src/components/BackgroundCacheCleaner.jsx
@@ -0,0 +1,12 @@
+import { useEffect } from "react";
+import { cleanCache } from "../lib/apiCache";
+
+const BackgroundCacheCleaner = () => {
+  useEffect(() => {
+    const id = setInterval(cleanCache, 60_000);
+    return () => clearInterval(id);
+  });
+  return null;
+};
+
+export default BackgroundCacheCleaner;
