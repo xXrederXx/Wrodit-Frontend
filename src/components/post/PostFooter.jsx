51c90f1542e78e5fdf276dbaa285755e99713937
xXrederXx <morgenthalerthierry@gmail.com>
2026-07-30 17:42:04 +0200

    feat: implement using vote bar

--- src/components/post/PostFooter.jsx
+++ src/components/post/PostFooter.jsx
@@ -5,49 +5,10 @@ import { useEffect, useState } from "react";
 import { DislikeLikePost, fetchSelfLikesPost, likePost, RemoveLikePost } from "../../lib/wrodit.js";
 
 import styles from "./PostFooter.module.css";
+import VoteBar from "../VoteBar.jsx";
 
 export default function PostFooter({ vote, postId }) {
-  const [userVote, setUserVote] = useState(0);
-
-  useEffect(() => {
-    const fetchLikes = async () => {
-      try {
-        const likeSelf = await fetchSelfLikesPost(postId, true);
-        setUserVote(likeSelf.vote || 0);
-      } catch (err) {
-        console.error(err.message || "Fehler beim Laden");
-      }
-    };
-    fetchLikes();
-  }, [postId]);
-
-  const handleLike = async value => {
-    const previous = userVote;
-
-    // optimistic update
-    setUserVote(value);
-
-    try {
-      await likePost(postId, value);
-    } catch (err) {
-      console.error(err);
-
-      // rollback if failed
-      setUserVote(previous);
-    }
-  };
-
   return (
-    <footer className={styles.footer}>
-      {vote + userVote}
-
-      {userVote === 1 ?
-        <FcLike onClick={() => handleLike(0)} />
-      : <AiOutlineHeart onClick={() => handleLike(1)} />}
-
-      {userVote === -1 ?
-        <AiFillDislike onClick={() => handleLike(0)} />
-      : <AiOutlineDislike onClick={() => handleLike(-1)} />}
-    </footer>
+    <VoteBar id={postId} totalVotes={vote} getOwnVote={fetchSelfLikesPost} postOwnVote={likePost} />
   );
 }
