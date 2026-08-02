0b44770c29b941c657ad97277021263eec00710a
xXrederXx <morgenthalerthierry@gmail.com>
2026-08-02 18:36:14 +0200

    fix: dropdown not closing when button clicked

--- src/components/ui/dropdown/DropdownMenuItem.jsx
+++ src/components/ui/dropdown/DropdownMenuItem.jsx
@@ -1,14 +1,18 @@
-import styles from "./DropdownMenuItem.module.css"
+import styles from "./DropdownMenuItem.module.css";
 
-export default function DropdownMenuItem({ children, onClick, active = false }) {
-    return (
-        <button
-            type="button"
-            onClick={onClick}
-            className={active ? styles.buttonActive : styles.button}
-            role="menuitem"
-        >
-            {children}
-        </button>
-    );
+export default function DropdownMenuItem({ children, onClick, closeMenu, active = false }) {
+  const handleClick = e => {
+    onClick?.(e);
+    closeMenu?.();
+  };
+
+  return (
+    <button
+      type="button"
+      onClick={handleClick}
+      className={active ? styles.buttonActive : styles.button}
+      role="menuitem">
+      {children}
+    </button>
+  );
 }
