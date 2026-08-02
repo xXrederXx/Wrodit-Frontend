94babf0055309a7c0742384c7196b18e7a48ea67
xXrederXx <morgenthalerthierry@gmail.com>
2026-08-02 17:29:32 +0200

    feat: move comment next to votes

--- src/components/comment/Comment.jsx
+++ src/components/comment/Comment.jsx
@@ -1,9 +1,4 @@
-import { Link } from "react-router-dom";
-
-import { getLoggedInUserId } from "../../lib/session.js";
-
 import styles from "./Comment.module.css";
-import CommentEditButton from "./CommentEditButton.jsx";
 import CommentFooter from "./CommentFooter.jsx";
 import CommentHeader from "./CommentHeader.jsx";
 
@@ -12,15 +7,7 @@ export default function Comment({ postId, data }) {
     <div className={styles.comment} style={{ flex: 10, minWidth: "20rem" }}>
       <CommentHeader data={data} />
       <p>{data.content}</p>
-      <CommentFooter vote={data.votes} commentId={data.id} />
-      <div>
-        <Link
-          to={`/wrodit/create/comment/parent/${postId}/${data.id}`}
-          className={styles.linkButton}>
-          Komentieren
-        </Link>
-        {data.user.id === getLoggedInUserId() && <CommentEditButton commentId={data.id} />}{" "}
-      </div>
+      <CommentFooter data={data} />
     </div>
   );
 }
--- src/components/comment/CommentFooter.jsx
+++ src/components/comment/CommentFooter.jsx
@@ -1,13 +1,23 @@
+import { Link } from "react-router-dom";
 import { fetchSelfLikesComment, likeComment } from "../../lib/wrodit.js";
 import VoteBar from "../VoteBar.jsx";
+import CommentEditButton from "./CommentEditButton.jsx";
+import { getLoggedInUserId } from "../../lib/session.js";
+import styles from "./CommentFooter.module.css";
 
-export default function CommentFooter({ vote, commentId }) {
+export default function CommentFooter({ postId, data }) {
   return (
-    <VoteBar
-      id={commentId}
-      totalVotes={vote}
-      getOwnVote={fetchSelfLikesComment}
-      postOwnVote={likeComment}
-    />
+    <div className={styles.container}>
+      <VoteBar
+        id={data.id}
+        totalVotes={data.votes}
+        getOwnVote={fetchSelfLikesComment}
+        postOwnVote={likeComment}
+      />
+      <Link to={`/wrodit/create/comment/parent/${postId}/${data.id}`} className={styles.linkButton}>
+        Komentieren
+      </Link>
+      {data.user.id === getLoggedInUserId() && <CommentEditButton commentId={data.id} />}{" "}
+    </div>
   );
 }
